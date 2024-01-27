2fc76463cfba4169a31459dcd9bf400134ca4e19
Josh Massa <joshmassa@gmail.com>
2024-01-26 20:38:19 -0500

    edited header, added bing map, added loadmap function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -174,3 +174,36 @@ function getLocationAndUpdateTitle() {
 document.addEventListener('DOMContentLoaded', function() {
     getLocationAndUpdateTitle();
 });
+
+
+
+
+
+
+
+
+function loadMapScenario() {
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        /* No need to set credentials if already passed in URL */
+        center: new Microsoft.Maps.Location(39.1887643719098, -92.8261546188403),
+        zoom: 4
+    });
+    //Set the map center default to the user's location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var userLocation = new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude);
+            map.setView({ center: userLocation, zoom: 9 });
+        });
+    //Update the map with a rain overlay
+    var urlTemplate = 'https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-{timestamp}/{zoom}/{x}/{y}.png';
+    var timestamps = ['900913-m50m', '900913-m45m', '900913-m40m', '900913-m35m', '900913-m30m', '900913-m25m', '900913-m20m', '900913-m15m', '900913-m10m', '900913-m05m', '900913'];
+    var tileSources = [];
+    for (var i = 0; i < timestamps.length; i++) {
+        var tileSource = new Microsoft.Maps.TileSource({
+            uriConstructor: urlTemplate.replace('{timestamp}', timestamps[i])
+        });
+        tileSources.push(tileSource);
+    }
+    var animatedLayer = new Microsoft.Maps.AnimatedTileLayer({ mercator: tileSources, frameRate: 500 });
+    map.layers.insert(animatedLayer);
+}};
